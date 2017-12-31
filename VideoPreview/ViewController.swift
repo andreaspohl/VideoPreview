b0b9e1509b7e5120af996bc18029d8d37c437b0c
andreaspohl <andreas.g.pohl@gmail.com>
2017-12-31 15:08:33 +0100

    feat: show video preview

--- VideoPreview/ViewController.swift
+++ VideoPreview/ViewController.swift
@@ -7,21 +7,82 @@
 //
 
 import Cocoa
+import AVFoundation
+import AVKit
 
 class ViewController: NSViewController {
-
+    
+    @IBOutlet var myView: NSView!
+    
+    
+    let session = AVCaptureSession()
+    let device = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
-    }
+        do {
+            
+            session.sessionPreset = AVCaptureSessionPresetLow
+            
+            try device?.lockForConfiguration()
+            
+            device?.focusMode = AVCaptureFocusMode.continuousAutoFocus
+            device?.exposureMode = AVCaptureExposureMode.continuousAutoExposure
+            //device?.whiteBalanceMode = AVCaptureWhiteBalanceMode.continuousAutoWhiteBalance
+            
+            device?.unlockForConfiguration()
+            
+            let deviceInput = try AVCaptureDeviceInput(device: device)
+            
+            if (session.canAddInput(deviceInput)) {
+                session.addInput(deviceInput)
+            }
+            
+            let captureOutput = AVCaptureVideoDataOutput()
+            
+            captureOutput.alwaysDiscardsLateVideoFrames = true
+            
+            let queue = DispatchQueue(label: "videoQueue")
+            
+            captureOutput.setSampleBufferDelegate(self, queue: queue)
+            
+            if (session.canAddOutput(captureOutput)) {
+                session.addOutput(captureOutput)
+            }
+            
+            let previewLayer = AVCaptureVideoPreviewLayer(session: session)
+            previewLayer?.videoGravity = AVLayerVideoGravityResizeAspectFill
+            
+            myView.layer = previewLayer!
+            myView.wantsLayer = true
+            
+            
+            session.startRunning()
 
+            
+        }
+        catch let error as NSError {
+            NSLog("\(error), \(error.localizedDescription)")
+        }
+        
+    }
+    
     override var representedObject: Any? {
         didSet {
-        // Update the view, if already loaded.
+            // Update the view, if already loaded.
         }
     }
+    
+    
+}
 
-
+extension ViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
+    
+    func captureOutput(_ captureOutput: AVCaptureOutput!, didOutputSampleBuffer sampleBuffer: CMSampleBuffer!, from connection: AVCaptureConnection!) {
+        //add code here
+    }
+    
 }
 
