eebd9709e54ce26eff17681f1995f19849e1c74d
andreaspohl <andreas.g.pohl@gmail.com>
2017-12-31 13:11:24 +0100

    Initial Commit

---
+++ VideoPreview/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  VideoPreview
+//
+//  Created by Andreas Pohl on 31.12.17.
+//  Copyright © 2017 Andreas Pohl. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ VideoPreview/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  VideoPreview
+//
+//  Created by Andreas Pohl on 31.12.17.
+//  Copyright © 2017 Andreas Pohl. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
